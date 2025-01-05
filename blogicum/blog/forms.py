323dbe21f18dcea96324f7e5ffc9058f04670039
A7med373 <a7medsaif2005@gmail.com>
2025-01-05 08:59:03 +0300

    Project Done!

---
+++ blogicum/blog/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+from .models import Post, Comment
+
+User = get_user_model()
+
+
+class EditProfileForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email']
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'text', 'pub_date', 'category', 'location', 'image']  # Add all necessary fields
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['text']
